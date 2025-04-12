90d85f76aaedda7979886f6f3b7e054e8b46ec27
siranjeevan <smartjeevith2k@gmail.com>
2025-04-12 11:36:22 +0530

    Initial Commit

---
+++ Billionaire simulation/Billionaire_simulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Billionaire_simulationApp.swift
+//  Billionaire simulation
+//
+//  Created by IOSTEAM   on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Billionaire_simulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Billionaire simulationTests/Billionaire_simulationTests.swift
@@ -0,0 +1,16 @@
+//
+//  Billionaire_simulationTests.swift
+//  Billionaire simulationTests
+//
+//  Created by IOSTEAM   on 4/12/25.
+//
+
+import Testing
+
+struct Billionaire_simulationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
